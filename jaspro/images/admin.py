e731f5c0b00bc3509228340c091a4d8f4e38a97e
wenuri <35003742+wenuri@users.noreply.github.com>
2018-01-06 18:56:18 +0700

    django user+comment+like

--- jaspro/images/admin.py
+++ jaspro/images/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image, Comment,Like
+
+
+
+@admin.register(Image)
+class AdminImage(admin.ModelAdmin) :
+
+    list_display = (
+        'caption',
+        'location',
+        'created_at',
+        'updated_at',
+        'author'
+    )
+
+    list_filter = (
+        'location',
+        'created_at'
+    )
+
+    search_fields = (
+        'location',
+    )
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin) :
+    pass
+
+@admin.register(Like)
+class Likes(admin.ModelAdmin) :
+    pass
--- jaspro/images/models.py
+++ jaspro/images/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Image(models.Model):
+
+    file = models.FileField()
+    caption = models.TextField()
+    location = models.CharField(max_length=50, default = '')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return f'{self.caption} - {self.location}'
+
+class Comment(models.Model):
+
+    message = models.TextField()
+    directed_to = models.ForeignKey(Image,on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.message} - {self.directed_to.location} comment by {self.created_by}'
+
+class Like(models.Model):
+
+    created_for = models.ForeignKey(Image,on_delete=models.CASCADE,null=True)
+    voted_by = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+
+    def __str__(self):
+        return f'{self.created_for} - {self.voted_by.username}'
